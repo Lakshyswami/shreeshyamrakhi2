119a0408ba2e41184d5df211c1dbcdf709eacdf2
Lakshyswami <lakshys371@gmail.com>
2026-07-11 22:03:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -709,6 +709,28 @@ function copyProductLink() {
 
 }
 
+function showToast(message) {
+
+    const toast = document.createElement("div");
+
+    toast.className = "toast";
+
+    toast.innerText = message;
+
+    document.body.appendChild(toast);
+
+    setTimeout(() => toast.classList.add("show"), 100);
+
+    setTimeout(() => {
+
+        toast.classList.remove("show");
+
+        setTimeout(() => toast.remove(), 300);
+
+    }, 2000);
+
+}
+
 // =======================
 // GLOBAL FUNCTIONS
 // =======================
